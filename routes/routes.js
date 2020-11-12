e9a276d1f1432e8323b1e6a7db730597edc018e6
mrcoro <aan_kunek@apps.ipb.ac.id>
2020-11-12 11:23:41 +0700

    commit kedua untuk CRUD inventory barang

--- routes/routes.js
+++ routes/routes.js
@@ -34,7 +34,10 @@ const createMenuItem = async (req, res, next) => {
       const newMenuItem = {
         menu_item_id: req.body.id,
         menu_item_name: req.body.name,
+        menu_item_category: req.body.category,
         menu_item_price: req.body.price,
+        menu_item_hpp: req.body.hpp,
+        menu_item_available: req.body.available,
         menu_item_promo: req.body.promo,
       };
 
@@ -66,7 +69,10 @@ const updateMenuItem = async (req, res, next) => {
       const updatedItemData = {
         menu_item_id: req.body.id,
         menu_item_name: req.body.name,
+        menu_item_category: req.body.category,
         menu_item_price: req.body.price,
+        menu_item_hpp: req.body.hpp,
+        menu_item_available: req.body.available,
         menu_item_promo: req.body.promo,
       };
 
@@ -122,5 +128,124 @@ const deleteMenuItem = async (req, res, next) => {
 
 router.route('/api/v1/menu_item/:id').delete(deleteMenuItem);
 
+//GET Inventory
+const getInventory = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, '/.././inventory.json'));
+    const inventory_item = JSON.parse(data);
+
+    if (!inventory_item) {
+      const err = new Error('Menu Items not found');
+      err.status = 404;
+      throw err;
+    } else {
+        res.json(inventory_item);
+    }
+
+  } catch (e) {
+    next(e);
+  }
+};
+
+router.route('/api/v1/inventory_item').get(getInventory);
+
+//POST METHOD untuk membuat Inventory
+const createInventoryItem = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, '/.././inventory.json'));
+    const inventory_item = JSON.parse(data);
+    
+    const newInventoryItem = {
+      inventory_item_id: req.body.id,
+      inventory_item_name: req.body.name,
+      inventory_item_amount: req.body.amount,
+      inventory_item_update: req.body.update,
+      inventory_item_alert: (req.body.amount < 5) ,
+    };
+
+    inventory_item.push(newInventoryItem);
+    fs.writeFileSync(__dirname + '/.././inventory.json', JSON.stringify(inventory_item));
+    res.status(201).json(newInventoryItem);
+  } catch (e) {
+    next(e);
+  }
+};
+
+router.route('/api/v1/inventory_item/create').post(createInventoryItem);
+
+
+//UPDATE Method untuk memodifikasi Inventory item yang sudah ada 
+const updateInventoryItem = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, '/.././inventory.json'));
+    const inventory_item = JSON.parse(data);
+    const findInventoryItem = inventory_item.find(item => item.inventory_item_id === Number(req.params.id));
+    
+    //tentukan apakah item ada atau tidak
+    if (!findInventoryItem) {
+      const err = new Error('Inventory item not found');
+      err.status = 404;
+      throw err;
+    }
+
+    const updatedInventoryItem = {
+      inventory_item_id: req.body.id,
+      inventory_item_name: req.body.name,
+      inventory_item_amount: req.body.amount,
+      inventory_item_update: req.body.update,
+      inventory_item_alert: (req.body.amount < 5) ,
+    };
+
+    const newStats = inventory_item.map(item => {
+      if (item.inventory_item_id === Number(req.params.id)) {
+        return updatedInventoryItem;
+      } else {
+        return item;
+      }
+    });
+
+    fs.writeFileSync(__dirname + '/.././inventory.json', JSON.stringify(newStats));
+    res.status(200).json(updatedInventoryItem);
+
+  } catch (e) {
+    next(e);
+  }
+
+};
+
+router.route('/api/v1/inventory_item/:id').put(updateInventoryItem);
+
+
+//DELETE METHOD untuk menghapus item
+const deleteInventoryItem = async (req, res, next) => {
+  try {
+    const data = fs.readFileSync(path.join(__dirname, '/.././inventory.json'));
+    const inventory_item = JSON.parse(data);
+    const findInventoryItem = inventory_item.find(item => item.inventory_item_id === Number(req.params.id));
+
+    if (!findInventoryItem) {
+      const err = new Error('Menu Item not found');
+      err.status = 404;
+      throw err;
+    }
+    
+    const newStats = inventory_item.map(item => {
+      if (item.inventory_item_id === Number(req.params.id)) {
+        return null;
+      } else {
+        return item;
+      }
+    })
+    .filter(item => item !== null);
+
+    fs.writeFileSync(__dirname + '/.././inventory.json', JSON.stringify(newStats));
+    res.status(200).end();
+
+  } catch (e) {
+    next(e);
+  }
+};
+
+router.route('/api/v1/inventory_item/:id').delete(deleteInventoryItem);
 
 module.exports = router;
